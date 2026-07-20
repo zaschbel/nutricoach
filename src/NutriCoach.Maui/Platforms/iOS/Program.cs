7c7b2a66b40381a1d47fb85712fac34e8062fade
zaschbel <213791700+zaschbel@users.noreply.github.com>
2026-07-20 21:53:34 +0200

    Add missing iOS platform entry point (Program.cs + AppDelegate.cs)
    Der iOS-Head hatte nie einen Main-Einstiegspunkt oder AppDelegate - Android
    und Windows hatten ihre Pendants, iOS fehlte komplett. Erklaert CS5001
    beim ersten echten iOS-Build.

---
+++ src/NutriCoach.Maui/Platforms/iOS/Program.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace NutriCoach.Maui;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
