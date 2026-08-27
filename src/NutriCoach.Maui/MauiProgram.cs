6d2e68fffc1a2dc5e67cc5c2397dc47702bb5d03
zaschbel <213791700+zaschbel@users.noreply.github.com>
2026-08-27 09:51:47 +0200

    Fix black corners on iOS by matching UIWindow background to dark theme

--- src/NutriCoach.Maui/MauiProgram.cs
+++ src/NutriCoach.Maui/MauiProgram.cs
@@ -1,7 +1,11 @@
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
 using Plugin.LocalNotification;
 using ZXing.Net.Maui;
 using ZXing.Net.Maui.Controls;
+#if IOS
+using UIKit;
+#endif
 
 namespace NutriCoach.Maui;
 
@@ -22,6 +26,21 @@ public static class MauiProgram
                 fonts.AddFont("Inter-Bold.ttf", "InterBold");
                 fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
             });
+
+#if IOS
+        // iOS' UIWindow ist standardmäßig reines Schwarz (#000000) hinterlegt. Da unsere feste
+        // Dunkelmodus-Palette (App.xaml.cs) nur SEHR dunkles Grau nutzt (#121316), fällt genau an
+        // den abgerundeten Display-Ecken - wo die eigene Seite nicht ganz bis zum physischen Rand
+        // reicht - dieser Farbunterschied als sichtbarer Schwarz-Fleck auf. Fix: Fensterhintergrund
+        // explizit auf denselben Farbton wie ColorBackground legen, damit es keinen Farbsprung gibt.
+        builder.ConfigureMauiHandlers(handlers =>
+        {
+            WindowHandler.Mapper.AppendToMapping("DarkWindowBackgroundForRoundedCorners", (handler, _) =>
+            {
+                handler.PlatformView.BackgroundColor = UIColor.FromRGB(0x12, 0x13, 0x16);
+            });
+        });
+#endif
 
 #if DEBUG
         builder.Logging.AddDebug();
